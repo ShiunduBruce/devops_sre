69b267b64fd819590fc8d46bc4d714dc0692c360
Bruce Shiundu <bruce.shiundu@sap.com>
2022-01-18 20:46:26 +0100

    demo

---
+++ main.py
@@ -0,0 +1,13 @@
+import git
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    repo = git.Repo("./")
+    return 'My last commit: ' + str(repo.head.commit)
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
