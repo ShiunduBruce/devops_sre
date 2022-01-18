9ccd53463b70cc5d17a0bd70065fd26cb00721d4
Bruce Shiundu <bruce.shiundu@sap.com>
2022-01-18 20:01:47 +0100

    demo

---
+++ demo.py
@@ -0,0 +1,10 @@
+import subprocess
+
+def get_git_revisions_hash():
+    hashes = []
+    hashes.append(subprocess.check_output(['git', 'rev-parse', 'HEAD']))
+    hashes.append(subprocess.check_output(['git', 'rev-parse', 'HEAD^']))
+    return hashes
+
+
+print(get_git_revisions_hash())
